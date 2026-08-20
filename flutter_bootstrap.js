df758fc56cf87130e054129c03963eb0f015d548
unicajp <masato.k0410x@gmail.com>
2026-08-20 02:02:06 +0000

    V16.8.1 Center town bottom navigation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2355110984" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2823281359" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
